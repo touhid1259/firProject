b3a0ccadeafd354ed0386345b57868b586be79fa
touhid1259 <MohammadTouhidur@fir.rwth-aachen.de>
2016-07-04 17:09:06 +0200

    executing page specific JS

--- app/assets/javascripts/control.js
+++ app/assets/javascripts/control.js
@@ -1,30 +1,33 @@
-$(document).ready(function() {
-  function drawTestGraph(gpitems){
-    var container = $(".graph")[0];
-    // var items = [
-    //   {x: '2014-06-11', y: 10},
-    //   {x: '2014-06-12', y: 25},
-    //   {x: '2014-06-13', y: 30},
-    //   {x: '2014-06-14', y: 10},
-    //   {x: '2014-06-15', y: 15},
-    //   {x: '2014-06-16', y: 30}
-    // ];
-    var items = gpitems
+if(window.location.pathname == '/controlling')
+{
+  $(document).ready(function() {
+    function drawTestGraph(gpitems){
+      var container = $(".graph")[0];
+      // var items = [
+      //   {x: '2014-06-11', y: 10},
+      //   {x: '2014-06-12', y: 25},
+      //   {x: '2014-06-13', y: 30},
+      //   {x: '2014-06-14', y: 10},
+      //   {x: '2014-06-15', y: 15},
+      //   {x: '2014-06-16', y: 30}
+      // ];
+      var items = gpitems
 
-    var dataset = new vis.DataSet(items);
-    var options = {
-      start: gpitems[0]['x'],
-      end: gpitems[9]['x'],
-      dataAxis: {
-        left: {
-          title: {
-            text: 'Spannung(Durchschnitt) in 0.01V'
+      var dataset = new vis.DataSet(items);
+      var options = {
+        start: gpitems[0]['x'],
+        end: gpitems[9]['x'],
+        dataAxis: {
+          left: {
+            title: {
+              text: 'Spannung(Durchschnitt) in 0.01V'
+            }
           }
         }
-      }
-    };
-    var graph2d = new vis.Graph2d(container, dataset, options);
-  }
+      };
+      var graph2d = new vis.Graph2d(container, dataset, options);
+    }
 
-  drawTestGraph(gon.graphData);
-});
+    drawTestGraph(gon.graphData);
+  });
+}
--- app/assets/javascripts/energy.js
+++ app/assets/javascripts/energy.js
@@ -1,23 +1,26 @@
-$(document).ready(function() {
-  $.simpleWeather({
-    // location: 'campus melaten, aachen, germany',
-    woeid: '20066562',
-    unit: 'c',
-    success: function(weather) {
-      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'<img src="' +weather.thumbnail+ '"></h2>';
-      html += '<ul><li><b>Location:</b> Campus Melaten, '+weather.city+', '+weather.region+'</li>';
-      html += '<li class="currently"><b>Current Condition:</b> '+weather.currently+'</li>';
-      html += '<li class="currently"><b>Humidity:</b> '+weather.humidity+'%</li>';
-      html += '<li><b>Wind Status:</b> '+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+if(window.location.pathname == '/energy')
+{
+  $(document).ready(function() {
+    $.simpleWeather({
+      // location: 'campus melaten, aachen, germany',
+      woeid: '20066562',
+      unit: 'c',
+      success: function(weather) {
+        html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'<img src="' +weather.thumbnail+ '"></h2>';
+        html += '<ul><li><b>Location:</b> Campus Melaten, '+weather.city+', '+weather.region+'</li>';
+        html += '<li class="currently"><b>Current Condition:</b> '+weather.currently+'</li>';
+        html += '<li class="currently"><b>Humidity:</b> '+weather.humidity+'%</li>';
+        html += '<li><b>Wind Status:</b> '+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
 
-      $(".weather").html(html);
-    },
-    error: function(error) {
-      $(".weather").html('<p>'+error+'</p>');
-    }
-  });
+        $(".weather").html(html);
+      },
+      error: function(error) {
+        $(".weather").html('<p>'+error+'</p>');
+      }
+    });
 
-  $('.fact').on('click', function(){
-    $('.choose-machine').dialog();
+    $('.fact').on('click', function(){
+      $('.choose-machine').dialog();
+    });
   });
-});
+}
