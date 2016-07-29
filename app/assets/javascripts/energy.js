2b6c5428258edc6994dd8acf349223a8cf4b8278
touhid1259 <touhid@gmail.com>
2016-07-29 17:16:03 +0200

    static graph with printer data

--- app/assets/javascripts/energy.js
+++ app/assets/javascripts/energy.js
@@ -1,26 +1,51 @@
-if(window.location.pathname == '/energy')
-{
-  $(document).ready(function() {
-    $.simpleWeather({
-      // location: 'campus melaten, aachen, germany',
-      woeid: '20066562',
-      unit: 'c',
-      success: function(weather) {
-        html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'<img src="' +weather.thumbnail+ '"></h2>';
-        html += '<ul><li><b>Location:</b> Campus Melaten, '+weather.city+', '+weather.region+'</li>';
-        html += '<li class="currently"><b>Current Condition:</b> '+weather.currently+'</li>';
-        html += '<li class="currently"><b>Humidity:</b> '+weather.humidity+'%</li>';
-        html += '<li><b>Wind Status:</b> '+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+$(document).ready(function() {
+  if(window.location.pathname == '/energy')
+  {
+      $.simpleWeather({
+        // location: 'campus melaten, aachen, germany',
+        woeid: '20066562',
+        unit: 'c',
+        success: function(weather) {
+          html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'<img src="' +weather.thumbnail+ '"></h2>';
+          html += '<ul><li><b>Location:</b> Campus Melaten, '+weather.city+', '+weather.region+'</li>';
+          html += '<li class="currently"><b>Current Condition:</b> '+weather.currently+'</li>';
+          html += '<li class="currently"><b>Humidity:</b> '+weather.humidity+'%</li>';
+          html += '<li><b>Wind Status:</b> '+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
 
-        $(".weather").html(html);
-      },
-      error: function(error) {
-        $(".weather").html('<p>'+error+'</p>');
+          $(".weather").html(html);
+        },
+        error: function(error) {
+          $(".weather").html('<p>'+error+'</p>');
+        }
+      });
+
+      $('.fact').on('click', function(){
+        $('.choose-machine').dialog();
+      });
+  }
+
+  if(window.location.pathname == '/energy/printer')
+  {
+      function drawPrinterGraph(gpitems){
+        var container = $(".printer-graph")[0];
+        var items = gpitems
+
+        var dataset = new vis.DataSet(items);
+        var options = {
+          start: gpitems[30]['x'],
+          end: gpitems[49]['x'],
+          dataAxis: {
+            left: {
+              title: {
+                text: 'Power'
+              }
+            }
+          }
+        };
+        var graph2d = new vis.Graph2d(container, dataset, options);
       }
-    });
 
-    $('.fact').on('click', function(){
-      $('.choose-machine').dialog();
-    });
-  });
-}
+      drawPrinterGraph(gon.graphData);
+  }
+
+});
--- app/controllers/energy_controller.rb
+++ app/controllers/energy_controller.rb
@@ -4,6 +4,13 @@ class EnergyController < ApplicationController
   end
 
   def printer_energy_data
-
+    graphData = Energy.last(50)
+    gon.graphData = graphData.collect do |item|
+      {
+        x: "#{item.date} " + "#{item.time.strftime('%H:%M:%S')}",
+        y: item.power
+      }
+    end
   end
+
 end
