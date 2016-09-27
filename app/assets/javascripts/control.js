88f0c82124b6653815b24d3f4f5b2efcc1e7ccf3
touhid1259 <touhid@gmail.com>
2016-09-27 17:50:30 +0200

    working on the controlling tab to visualize machines data

--- app/assets/javascripts/control.js
+++ app/assets/javascripts/control.js
@@ -1,14 +1,32 @@
 $(document).on("turbolinks:load", function() {
   if(window.location.pathname == '/controlling')
   {
+      var groups = new vis.DataSet();
+      var graph2d;
+
       function drawTestGraph(gpitems){
         var container = $(".graph")[0];
         var items = gpitems
+        var machines = ["HF_Laser", "AbsauganlageLaser", "KuehlungLaser", "MechanikLaser", "Druckluft"];
+        for(i = 0; i < 5; i++){
+          groups.add({
+            id: machines[i],
+            content: machines[i]
+          })
+        }
 
         var dataset = new vis.DataSet(items);
         var options = {
           start: gpitems[0]['x'],
-          end: gpitems[9]['x'],
+          end: gpitems[49]['x'],
+          drawPoints: {
+            style: 'circle', // square, circle
+            size: 7
+          },
+          legend: true,
+          shaded: {
+            orientation: 'bottom' // top, bottom
+          },
           dataAxis: {
             left: {
               title: {
@@ -17,9 +35,30 @@ $(document).on("turbolinks:load", function() {
             }
           }
         };
-        var graph2d = new vis.Graph2d(container, dataset, options);
+        graph2d = new vis.Graph2d(container, dataset, groups ,options);
       }
 
       drawTestGraph(gon.graphData);
+
+      $(".machine-checkbox > label > input").on('change', function(){
+        var cls = $(this).attr("class");
+        if($(this).is(":checked")){
+          graph2d.setOptions({
+              groups:{
+                  visibility:{
+                      "HF_Laser": true
+                  }
+              }
+          });
+        } else {
+           graph2d.setOptions({
+               groups:{
+                   visibility:{
+                       "HF_Laser": false
+                   }
+               }
+           });
+       }
+      });
   }
 });
--- app/controllers/control_controller.rb
+++ app/controllers/control_controller.rb
@@ -1,11 +1,12 @@
 class ControlController < ApplicationController
 	def index
 		# @tables = DfaDbConnection.connection.tables
-		graphData = TbWeidmuellerHf2.select("Datum, Zeit, MaschinenID, Spannung_Durchschnitt_in_0_01V").limit(300)
+		graphData = TbWeidmuellerHf2.select("Datum, Zeit, MaschinenID, Spannung_Durchschnitt_in_0_01V").limit(5000)
 		gon.graphData = graphData.collect do |item|
 			{
 				x: "#{item.Datum} " + "#{item.Zeit.strftime('%H:%M:%S')}",
-				y: item["Spannung_Durchschnitt_in_0_01V"]
+				y: item["Spannung_Durchschnitt_in_0_01V"],
+				group: item.MaschinenID,
 			}
 		end
 	end
