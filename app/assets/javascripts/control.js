e9b6f1340388d9757ccacad2771cac696167b25b
touhid1259 <touhid@gmail.com>
2016-11-03 14:14:15 +0100

    adding bar graph for storm values in dfa database

--- app/assets/javascripts/control.js
+++ app/assets/javascripts/control.js
@@ -5,7 +5,7 @@ $(document).on("turbolinks:load", function() {
       var graph2d;
       var machines = ["HF_Laser", "AbsauganlageLaser", "KuehlungLaser", "MechanikLaser", "Druckluft"];
 
-      function drawTestGraph(gpitems){
+      function drawSpannungGraph(gpitems){
         var container = $(".graph")[0];
         var items = gpitems;
         for(i = 0; i < 5; i++){
@@ -38,7 +38,45 @@ $(document).on("turbolinks:load", function() {
         graph2d = new vis.Graph2d(container, dataset, groups ,options);
       }
 
-      drawTestGraph(gon.graphData);
+      var stormGroups = new vis.DataSet();
+      var stormGraph2d;
+
+      function drawStormGraph(gpitems){
+        var container = $("#storm-graph")[0];
+        var items = gpitems;
+        for(i = 0; i < 5; i++){
+          stormGroups.add({
+            id: machines[i],
+            content: machines[i]
+          })
+        }
+
+        var dataset = new vis.DataSet(items);
+        var options = {
+          start: gpitems[0]['x'],
+          end: gpitems[49]['x'],
+
+          style: 'bar',
+          stack:false,
+          barChart: {width:50, align:'center', sideBySide:true},
+          legend: true,
+          drawPoints: false,
+          shaded: {
+            orientation: 'bottom' // top, bottom
+          },
+          dataAxis: {
+            left: {
+              title: {
+                text: 'Strom(Durchschnitt) in 0.001A'
+              }
+            }
+          }
+        };
+        stormGraph2d = new vis.Graph2d(container, dataset, stormGroups ,options);
+      }
+
+      drawSpannungGraph(gon.spannungData);
+      drawStormGraph(gon.stormData);
 
       $(".machine-checkbox > label > input").on('change', function(){
         var cls = $(this).attr("class");
--- app/controllers/control_controller.rb
+++ app/controllers/control_controller.rb
@@ -2,7 +2,20 @@ class ControlController < ApplicationController
 	def index
 		# @tables = DfaDbConnection.connection.tables
 		graphData = TbWeidmuellerHf2.limit(5000)
-		gon.graphData = graphData.collect do |item|
+		gon.stormData = []
+		gon.spannungData = graphData.collect do |item|
+			gon.stormData << {
+				x: "#{item.Datum} " + "#{item.Zeit.strftime('%H:%M:%S')}",
+				y: item["Strom_Durchschnitt_in_0_001A"],
+				group: item.MaschinenID,
+				label: {
+					content: item["Strom_Durchschnitt_in_0_001A"],
+					className: "point-label",
+					xOffset: -10,
+					yOffset: -10
+				}
+			}
+
 			{
 				x: "#{item.Datum} " + "#{item.Zeit.strftime('%H:%M:%S')}",
 				y: item["Spannung_Durchschnitt_in_0_01V"],
@@ -11,7 +24,7 @@ class ControlController < ApplicationController
 					content: item["Spannung_Durchschnitt_in_0_01V"],
 					className: "point-label",
 					xOffset: -10,
-					yOffset: -10,
+					yOffset: -10
 				}
 			}
 		end
