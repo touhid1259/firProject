ca038e959aa95e4eaa023b9cdc6133aad7d27567
touhid1259 <touhid@gmail.com>
2017-03-01 16:01:28 +0100

    adding design changes for the printer real time data

--- app/assets/javascripts/energy.js
+++ app/assets/javascripts/energy.js
@@ -155,14 +155,14 @@ $(document).on("turbolinks:load", function() {
           groups.add({
               id: i,
               content: 'groups',
-              className: i == 0 ? '' : 'high-datewise-data'
+              className: i == 0 ? 'datewise-data' : 'high-datewise-data'
           });
         }
 
         dataset = new vis.DataSet(items);
         var options = {
-          start: gpitems[30]['x'],
-          end: gpitems[49]['x'],
+          start: gpitems[32]['x'],
+          end: new Date(new Date(gpitems[49]['x']).getTime() + 2000),
           // end: gpitems[4]['x'],
           interpolation: false,
           drawPoints: {
@@ -170,18 +170,20 @@ $(document).on("turbolinks:load", function() {
               if(item.y > 99){
                 return {
                   style: 'circle',
-                  size: 8
+                  size: 5
                   // className: 'power-more-20'
                 }
 
               }else {
                 return {
-                  style: 'circle'
+                  style: 'circle',
+                  size: 4
                 }
 
               }
             },
             style: 'circle', // square, circle
+            size: 4
           },
           shaded: {
             orientation: 'bottom' // top, bottom
@@ -223,7 +225,8 @@ $(document).on("turbolinks:load", function() {
             content: con,
             className: "lb", xOffset: -7, yOffset: -10
           },
-          group: 0
+          group: 0,
+          className: "datewise-data"
         });
 
         if(power > 99) {
