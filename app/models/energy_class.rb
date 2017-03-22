c50afb7792c2c979e18de7e9e7b9f31eed9e333c
touhid1259 <touhid@gmail.com>
2017-03-22 15:17:42 +0100

    adding legend for the color coding in the graph

--- app/models/energy_class.rb
+++ app/models/energy_class.rb
@@ -1,4 +1,16 @@
 class EnergyClass < ActiveRecord::Base
   self.table_name = 'printer.energy_class'
   scope :get_cluster_ids, -> (start_time, end_time) {where("datetime >= ? and datetime <= ?", start_time, end_time)}
+
+  COLOR_CODES = {
+    'cluster_1' => '#006dff',
+    'cluster_2' => '#01a737',
+    'cluster_3' => '#f92b71',
+    'cluster_4' => '#dcc600',
+    'cluster_5' => '#a776ff',
+    'cluster_6' => '#f97421',
+    'cluster_7' => '#795548',
+    'cluster_8' => '#de2afd'
+  }
+
 end
