df37865f2d6dab4cd0aaa2588818d7b1ae63a043
touhid1259 <touhid@gmail.com>
2017-05-03 17:20:23 +0200

    fixing the google graph view of printer stream

--- app/controllers/energy_controller.rb
+++ app/controllers/energy_controller.rb
@@ -227,25 +227,25 @@ class EnergyController < ApplicationController
   end
 
   def googleGraph_printer_energy_data
-    graphData = Energy.last(10)
-    printer_status_data = {}
-    cluster_ids = {}
-    start_datetime = graphData.first.datetime
-    end_datetime = graphData.last.datetime
-    Status.status_of(start_datetime, end_datetime).collect do |item|
-      printer_status_data[item.timestamp] = item.printer_status
-    end
+    graphData = EnergyClass.last(10)
+    # printer_status_data = {}
+    # cluster_ids = {}
+    # start_datetime = graphData.first.datetime
+    # end_datetime = graphData.last.datetime
+    # Status.status_of(start_datetime, end_datetime).collect do |item|
+    #   printer_status_data[item.timestamp] = item.printer_status
+    # end
 
-    EnergyClass.get_cluster_ids(start_datetime, end_datetime).collect do |item|
-      cluster_ids[item.datetime] = item.cluster_id
-    end
+    # EnergyClass.get_cluster_ids(start_datetime, end_datetime).collect do |item|
+    #   cluster_ids[item.datetime] = item.cluster_id
+    # end
 
     gon.graphData = graphData.collect{ |item|
       [
         item.datetime.strftime("%H:%M:%S"),
         item.power,
-        "color: #{EnergyClass::COLOR_CODES['cluster_' + cluster_ids[item.datetime].to_s]}",
-        Status::PRINTER_STATUS[printer_status_data[item.datetime]]
+        "color: #{EnergyClass::COLOR_CODES['cluster_' + item.cluster_id.to_s]}",
+        Status::PRINTER_STATUS[Status::PRINTER_STATUS_KEYS[item.state_category]]
       ]
     }
   end
@@ -257,13 +257,13 @@ class EnergyController < ApplicationController
 
       last_data_time = -1
       loop do
-        Energy.uncached do
-          item = Energy.last
+        EnergyClass.uncached do
+          item = EnergyClass.last
           sleep 0.5
-          printer_status_data = Status.where(timestamp: item.datetime).take
-          label_text = printer_status_data.nil? ? nil : Status::PRINTER_STATUS[printer_status_data.printer_status]
-          cluster_id = EnergyClass.where(datetime: item.datetime).take
-          cluster_color = cluster_id.nil? ? "black" : EnergyClass::COLOR_CODES['cluster_' + cluster_id.cluster_id.to_s]
+          # printer_status_data = Status.where(timestamp: item.datetime).take
+          label_text = Status::PRINTER_STATUS[Status::PRINTER_STATUS_KEYS[item.state_category]]
+          # cluster_id = EnergyClass.where(datetime: item.datetime).take
+          cluster_color = EnergyClass::COLOR_CODES['cluster_' + item.cluster_id.to_s]
 
           if(last_data_time != item.datetime)
             sse.write({
