ff78c5383ff748a8d8b1bbd89b5256c023cb58a8
touhid1259 <touhid@gmail.com>
2017-12-13 15:33:17 +0100

    design tweaks

--- app/helpers/ewima_helper.rb
+++ app/helpers/ewima_helper.rb
@@ -1,2 +1,9 @@
 module EwimaHelper
+  def repeat_html_code(times, code)
+    repeated_code = code
+    (1..times).each do |item|
+      repeated_code = repeated_code + code
+    end
+    return repeated_code
+  end
 end
